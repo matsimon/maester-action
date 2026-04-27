044bcedc289bcd7947099b29e586ee9ea9975f23
Mathieu Simon <mathieu.simon@iseag.ch>
2026-04-27 14:17:42 +0200

    Update Run-MaesterAction.ps1

--- script/Run-MaesterAction.ps1
+++ script/Run-MaesterAction.ps1
@@ -161,7 +161,7 @@ PROCESS {
         Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force
         Import-Module ExchangeOnlineManagement
 
-        #$outlookToken = Get-MtAccessTokenUsingCli -ResourceUrl 'https://outlook.office365.com'
+        $outlookToken = Get-MtAccessTokenUsingCli -ResourceUrl 'https://outlook.office365.com'
         Connect-IPPSSession -AccessToken $outlookToken -AppId $ClientId -Organization $TenantId -ShowBanner:$false
         Write-Host "✔️ Security & Compliance connected."
     } else {
